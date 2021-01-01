cbb03c841458169f689d3c6ce68d9c0111b5038c
Kayo Elias <contatodevkayo@gmail.com>
2021-01-01 20:02:53 -0300

    Primeira Aplicação em NextJS

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>Testando a primeira aplicação em NextJS</h1>
+      <p>Hello, World!</p>
+    </>
+  );
+}
+
+export default Home;
